6714f0814319465caa1d2d1154bed0d373dbd709
LavaOlsen <79588155+LavaOlsen@users.noreply.github.com>
2021-09-20 11:34:58 +0200

    Printed pixels fra 0-100

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import cv2
+import numpy as np
 
 picture0 = cv2.imread("1.jpg")
-cv2.imshow("glemdet" , picture0)
-cv2.waitKey(0)
 
+
+
+#Converter RGB til HSV
+cv2.COLOR_BGR2HSV
+
+
+
+#for y, col in enumerate picture0:
+    #for x, px in enumerate col:
+
+for y in range(0, 100):
+    for x in range(0, 100):
+        print(f"pixel value at ({x}, {y}): {picture0[y,x]}")
+
+
+
+cv2.waitKey(0)
