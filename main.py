2c63f25ef8eaef6b86e756e24837e750c631fca7
tokilava <tokilava@gmail.com>
2021-09-21 19:19:29 +0200

    converterer det originale billede til HSV

--- main.py
+++ main.py
@@ -2,18 +2,37 @@ import cv2
 import numpy as np
 
 picture0 = cv2.imread("1.jpg")
-cropped_picture = picture0[0:100, 100:200]
-cv2.imshow("Cropped image", cropped_picture)
-cv2.imshow("original", picture0)
+picture1 = cv2.cvtColor(picture0, cv2.COLOR_BGR2HSV)
+cropped_picture = picture1[0:100, 0:100]
+
+
+cv2.imshow("1", picture0)
+cv2.imshow("2", picture1)
+cv2.imshow("3", cropped_picture)
+avg_HSV = np.average(picture1, axis=0)
+avg_HsV = np.average(avg_HSV, axis=0)
+print(avg_HsV)
+#cv2.imshow("Cropped image", cropped_picture)
+#cv2.imshow("original", picture0)
+
+
+
 
 #average RGB i croppede billede
 avg_col = np.average(cropped_picture, axis=0)
 avg_color = np.average(avg_col, axis = 0)
 print(f"avg_color: {avg_color}")
 
+#En prøve i at convertere billedet til HSV og så ændre V værdien
+#picture1 = cv2.imread("1.jpg")
+#picture1hsv = cv2.cvtColor(picture1, cv2.COLOR_BGR2HSV)
+#mask = cv2.inRange(picture1hsv,(0, 0, 0), (180, 255, 255))
+#cv2.imshow("picture1", mask)
 
 #Converter RGB til HSV
-cv2.COLOR_BGR2HSV
+#cv2.COLOR_BGR2HSV
+
+
 
 
 
@@ -27,3 +46,4 @@ cv2.COLOR_BGR2HSV
 
 
 cv2.waitKey(0)
+cv2.destroyAllWindows()
