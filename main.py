9ea95c242eb532a3de938effcc816f82a48aa965
tokilava <tokilava@gmail.com>
2021-09-26 22:36:36 +0200

    har lavet contours rundt om vand brikkerne, har lavet ranges for de forskellige tiles men mangler stadig nogle der ikke er med i det første billede

--- main.py
+++ main.py
@@ -2,23 +2,67 @@ import cv2
 import numpy as np
 
 picture0 = cv2.imread("1.jpg")
-picture1 = cv2.cvtColor(picture0, cv2.COLOR_BGR2HSV)
-cropped_picture = picture1[0:100, 0:100]
+blurredPicture = cv2.GaussianBlur(picture0, (5, 5), 0)
+picture1 = cv2.cvtColor(blurredPicture, cv2.COLOR_BGR2HSV) # Converts the first picture to HSV
+
+#Ranges of the certain tiles within the game
+grass_lowRange = np.array([35, 154, 140])
+grass_upperRange = np.array([50, 249, 176])
+water_lowRange = np.array([95, 175, 68])
+water_upperRange = np.array([111, 255, 255])
+forest_lowRange = np.array([30, 0, 0])
+forest_upperRange = np.array([87, 255, 120])
+sand_lowRange = np.array([22, 230, 181])
+sand_upperRange = np.array([31, 255, 255])
+desert_lowRange = np.array([0, 0, 50])
+desert_upperRange = np.array([30, 255, 112])
+crown_lowRange = np.array([0, 0, 151])
+crown_upperRange = np.array([35, 217, 195])
+
+#masks that cover the certain ranges of the tiles
+grass_mask = cv2.inRange(picture1, grass_lowRange, grass_upperRange)
+water_mask = cv2.inRange(picture1, water_lowRange, water_upperRange)
+forest_mask = cv2.inRange(picture1, forest_lowRange, forest_upperRange)
+sand_mask = cv2.inRange(picture1, sand_lowRange, sand_upperRange)
+desert_mask = cv2.inRange(picture1, desert_lowRange, desert_upperRange)
+crown_mask = cv2.inRange(picture1, crown_lowRange, crown_upperRange)
 
 
-cv2.imshow("RGB", picture0)
-cv2.imshow("HSV", picture1)
-
-lower_range = np.array([35, 154, 140])
-upper_range = np.array([50, 249, 176])
+#contours
 
 
 
-mask = cv2.inRange(picture1, lower_range, upper_range)
-cropped_picture = mask[0:100, 0:100]
+while True:
+#masks displayed on the screen
+    #cv2.imshow("grass", grass_mask)
+
+    #cv2.imshow("forest", forest_mask)
+    #cv2.imshow("sand", sand_mask)
+    #cv2.imshow("desert", desert_mask)
+    #cv2.imshow("crown", crown_mask)
+
+
+
+    #add contours around the tiles, and apply a white line around the given tiles
+    contours,hierarchy = cv2.findContours(water_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    #for contour in contours: #prøver at kun tegne contours rundt om de store brikker
+        #area = cv2.contourArea(contour)
+        #if area > 5000:
+            #cv2.drawContours(picture0, contour, -1, (255, 255, 255), 3)
+
+    cv2.drawContours(picture0, contours, -1, (255, 255, 255), 3)
+    cv2.imshow("water", water_mask)
+    cv2.imshow("RGB", picture0)
+
+    key = cv2.waitKey(1) #when the user presses esc key, the program shuts down
+    if key == 27:
+        break
+cv2.destroyAllWindows()
+
+
+cropped_picture = grass_mask[0:100, 0:100]
 cv2.imshow("Cropped", cropped_picture)
-cv2.imshow("Grass Tiles", mask)
-#cv2.imshow("Cropped image", cropped_picture)
+#cv2.imshow("Cropped image", cropped_picture)q
 #cv2.imshow("original", picture0)
 
 
@@ -51,5 +95,4 @@ print(f"avg_color: {avg_color}")
 
 
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
