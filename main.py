a0281c72b0652cb1598ade9905b0e8e99c2844f1
LavaOlsen <79588155+LavaOlsen@users.noreply.github.com>
2021-10-07 11:46:01 +0200

    har lavet for loop som iterate over hvert 100 pixel, også tilføjer vi de forskellige mask til hvert billede og printer deres værdi hvis de er ovre 60 i avg pixel col

--- main.py
+++ main.py
@@ -27,22 +27,30 @@ sand_mask = cv2.inRange(picture1, sand_lowRange, sand_upperRange)
 desert_mask = cv2.inRange(picture1, desert_lowRange, desert_upperRange)
 crown_mask = cv2.inRange(picture1, crown_lowRange, crown_upperRange)
 
+kernel = np.ones((7, 7), np.uint8)
+morph_grass = cv2.morphologyEx(forest_mask, cv2.MORPH_OPEN, kernel)
+cv2.imshow("morph", morph_grass)
 
-#contours
+mask_list = [grass_mask, water_mask, forest_mask, sand_mask , desert_mask, crown_mask]
+
+#iterate over each 100 pixel in and print out the average pixel value if its above or equal to 50
+counter = 0 # Variable for counting means
+for y in range(0, 500, 100):
+    for x in range(0, 500, 100):
+        for mask in mask_list:
+            tile = mask[y: y+100, x: x+100]
+            if np.average(tile) >= 60:
+                counter += 1
+                print(f"Mean {counter}: {np.average(tile)}")
+
+
+
+
+
 
 
 
 while True:
-#masks displayed on the screen
-    #cv2.imshow("grass", grass_mask)
-
-    #cv2.imshow("forest", forest_mask)
-    #cv2.imshow("sand", sand_mask)
-    #cv2.imshow("desert", desert_mask)
-    #cv2.imshow("crown", crown_mask)
-
-
-
     #add contours around the tiles, and apply a white line around the given tiles
     contours,hierarchy = cv2.findContours(water_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     #for contour in contours: #prøver at kun tegne contours rundt om de store brikker
@@ -50,9 +58,15 @@ while True:
         #if area > 5000:
             #cv2.drawContours(picture0, contour, -1, (255, 255, 255), 3)
 
-    cv2.drawContours(picture0, contours, -1, (255, 255, 255), 3)
+    cv2.drawContours(picture1, contours, -1, (255, 255, 255), 3)
+    cv2.imshow("grass", grass_mask)
     cv2.imshow("water", water_mask)
-    cv2.imshow("RGB", picture0)
+    cv2.imshow("sand", sand_mask)
+    cv2.imshow("HSV", picture1)
+    cv2.imshow("forest", forest_mask)
+            #avg_col = np.average(picture1, axis=None)
+            #avg_color = np.average(avg_col, axis=None)
+            #print(f"avg_color: {avg_color}")
 
     key = cv2.waitKey(1) #when the user presses esc key, the program shuts down
     if key == 27:
@@ -65,29 +79,19 @@ cv2.imshow("Cropped", cropped_picture)
 #cv2.imshow("Cropped image", cropped_picture)q
 #cv2.imshow("original", picture0)
 
+#for y in 100(picture1):
+ #   for x in 100(picture1):
+
 
 
 
 #average RGB i croppede billede
-avg_col = np.average(cropped_picture, axis=None)
-avg_color = np.average(avg_col, axis = None)
-print(f"avg_color: {avg_color}")
+#avg_col = np.average(cropped_picture, axis=None)
+#avg_color = np.average(avg_col, axis = None)
+#print(f"avg_color: {avg_color}")
 
-#En prøve i at convertere billedet til HSV og så ændre V værdien
-#picture1 = cv2.imread("1.jpg")
-#picture1hsv = cv2.cvtColor(picture1, cv2.COLOR_BGR2HSV)
-#mask = cv2.inRange(picture1hsv,(0, 0, 0), (180, 255, 255))
-#cv2.imshow("picture1", mask)
-
-#Converter RGB til HSV
-#cv2.COLOR_BGR2HSV
-
-
-
-
-
-#for y, col in enumerate picture0:
-    #for x, px in enumerate col:
+#for y, row in enumerate (picture0): #med enumerate får vi også koordinaterne med
+    #for x, pixel in enumerate (row):
 
 #for y in range(0, 100):
     #for x in range(0, 100):
