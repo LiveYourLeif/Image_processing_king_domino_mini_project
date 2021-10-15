7954f4be0963aab3790d22edd7e359422e202a7b
LavaOlsen <79588155+LavaOlsen@users.noreply.github.com>
2021-10-15 21:19:22 +0200

    algoritmen kan nu aflæse hvad for nogle tiles det er og sætte dem ret ind i en tilhørende matrice

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ picture0 = cv2.imread("1.jpg")
 blurredPicture = cv2.GaussianBlur(picture0, (5, 5), 0)
 picture1 = cv2.cvtColor(blurredPicture, cv2.COLOR_BGR2HSV) # Converts the first picture to HSV
 maskMatrix = np.zeros((5, 5))
-print(maskMatrix)
+
 
 
 #Ranges of the certain tiles within the game
@@ -37,42 +37,30 @@ morph_forest = cv2.morphologyEx(forest_mask, cv2.MORPH_OPEN, kernel)
 cv2.imshow("morph", morph_forest)
 
 mask_list = [grass_mask, water_mask, morph_forest, sand_mask, desert_mask, crown_mask]
-mask_names = ["Grass tile", "Water tile", "Forest tile", "Sand tile", "Desert tile", "Crown tile"]
-
 
 
 # iterate over each 100 pixel in and print out the average pixel value if its above or equal to 60
-counter = 0  # Variable for counting means
-
-
-#for y in range(0, 500, 100):
-    #for x in range(0, 500, 100):
-        #for mask in mask_list:
-            #tile = mask[y: y+100, x: x+100]
-            #if np.average(tile) >= 60:
-                #print(f"Mean {counter}: {np.average(tile)}")
-                #counter += 1
-
-
 maskNumber = 0
+
 for mask in mask_list:
     maskNumber = maskNumber + 1
+    y1 = 0
     for y in range(0, 500, 100):
+        y1 = y1 + 1
+        x1 = 0
         for x in range(0, 500, 100):
+            x1 = x1 + 1
             tile = mask[y: y + 100, x: x + 100]
             if np.average(tile) >= 60:
                 print(f"{maskNumber}: {np.average(tile)}")
-                #counter += 1
-                #print(f"Mean {counter}: {np.average(tile)} {mask_list == True} ")
-
+                maskMatrix[y1-1, x1-1] = maskNumber
+#print(y1, x1)
+print(maskMatrix)
 
 
 
 
 #contours
-
-
-
 while True:
 #masks displayed on the screen
     cv2.imshow("grass", grass_mask)
@@ -99,41 +87,3 @@ while True:
     if key == 27:
         break
 cv2.destroyAllWindows()
-
-
-cropped_picture = grass_mask[0:100, 0:100]
-cv2.imshow("Cropped", cropped_picture)
-#cv2.imshow("Cropped image", cropped_picture)q
-#cv2.imshow("original", picture0)
-
-
-
-
-#average RGB i croppede billede
-avg_col = np.average(cropped_picture, axis=None)
-avg_color = np.average(avg_col, axis = None)
-print(f"avg_color: {avg_color}")
-
-#En prøve i at convertere billedet til HSV og så ændre V værdien
-#picture1 = cv2.imread("1.jpg")
-#picture1hsv = cv2.cvtColor(picture1, cv2.COLOR_BGR2HSV)
-#mask = cv2.inRange(picture1hsv,(0, 0, 0), (180, 255, 255))
-#cv2.imshow("picture1", mask)
-
-#Converter RGB til HSV
-#cv2.COLOR_BGR2HSV
-
-
-
-
-
-#for y, col in enumerate picture0:
-    #for x, px in enumerate col:
-
-#for y in range(0, 100):
-    #for x in range(0, 100):
-        #print(f"pixel value at ({x}, {y}): {picture0[y,x]}")
-
-
-
-
