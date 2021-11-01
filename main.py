2de192cccc7266c37699aeb337538d939487b501
tokilava <tokilava@gmail.com>
2021-11-01 16:05:46 +0100

    Lavet kommentarer til det sidste

--- main.py
+++ main.py
@@ -5,20 +5,19 @@ import templateMatching
 
 
 '''
-Below the first picture is read, blurred through Gaussian blur and corverted to HSV colors.
+Below the first picture is read, blurred through Gaussian blur and converted to HSV colors.
 In addition we create an empty 5x5 matrix, which is used later on to perform the Grassfire algorithm.
 '''
-picture0 = cv2.imread("Images/2.jpg")
-blurredPicture = cv2.GaussianBlur(picture0, (5, 5), 0)
-picture1 = cv2.cvtColor(blurredPicture, cv2.COLOR_BGR2HSV)
-maskMatrix = np.zeros((5, 5), dtype=np.uint8)
-cv2.imshow("Maa", maskMatrix)
+inputPicture = cv2.imread("Images/3.jpg")
+blurredPicture = cv2.GaussianBlur(inputPicture, (5, 5), 0)
+HSVPicture = cv2.cvtColor(blurredPicture, cv2.COLOR_BGR2HSV)
+
 
 
 
 
 '''
-Below we define the lower and upper-range HSV values to the their associated tile eg. grass etc.
+Below we define the lower and upper-range HSV values to the their associated tile eg. grass, forest etc.
 '''
 grassLowRange = np.array([35, 154, 140])
 grassUpperRange = np.array([50, 249, 176])
@@ -34,7 +33,7 @@ mineLowerRange = np.array([0, 0, 0])
 mineUpperRange = np.array([20, 88, 61])
 crownLowRange = np.array([0, 0, 85])
 crownUpperRange = np.array([28, 91, 243])
-#yo
+
 
 
 
@@ -42,13 +41,13 @@ crownUpperRange = np.array([28, 91, 243])
 '''
 Below we define a mask for each tile matching their HSV values.
 '''
-grassMask = cv2.inRange(picture1, grassLowRange, grassUpperRange)
-waterMask = cv2.inRange(picture1, waterLowRange, waterUpperRange)
-forestMask = cv2.inRange(picture1, forestLowRange, forestUpperRange)
-sandMask = cv2.inRange(picture1, sandLowRange, sandUpperRange)
-desertMask = cv2.inRange(picture1, desertLowRange, desertUpperRange)
-mineMask = cv2.inRange(picture1, mineLowerRange, mineUpperRange)
-crownMask = cv2.inRange(picture1, crownLowRange, crownUpperRange)
+grassMask = cv2.inRange(HSVPicture, grassLowRange, grassUpperRange)
+waterMask = cv2.inRange(HSVPicture, waterLowRange, waterUpperRange)
+forestMask = cv2.inRange(HSVPicture, forestLowRange, forestUpperRange)
+sandMask = cv2.inRange(HSVPicture, sandLowRange, sandUpperRange)
+desertMask = cv2.inRange(HSVPicture, desertLowRange, desertUpperRange)
+mineMask = cv2.inRange(HSVPicture, mineLowerRange, mineUpperRange)
+crownMask = cv2.inRange(HSVPicture, crownLowRange, crownUpperRange)
 
 
 
@@ -57,7 +56,8 @@ crownMask = cv2.inRange(picture1, crownLowRange, crownUpperRange)
 We decided to apply morphology to the various tiles in order to reduce noise.
 First we created a kernel with the size of 7x7 , and applied compound operations to reduce noise
 surrounding the different masks. The compound operations(Open & close) are chosen by how each tile type has different
-noisy elements in their design, such as houses, boats etc.
+noisy elements in their design, such as houses, boats etc. Some of the masks have been morphed twice, 
+to further reduce noise
 '''
 kernel = np.ones((7, 7), np.uint8)
 morphedGrassMask = cv2.morphologyEx(grassMask, cv2.MORPH_CLOSE, kernel)
@@ -65,7 +65,6 @@ morphedWaterMask = cv2.morphologyEx(waterMask, cv2.MORPH_CLOSE, kernel)
 morphedForestMask = cv2.morphologyEx(forestMask, cv2.MORPH_OPEN, kernel)
 morphedSandMask = cv2.morphologyEx(sandMask, cv2.MORPH_CLOSE, kernel)
 morphedDesertMask = cv2.morphologyEx(desertMask, cv2.MORPH_CLOSE, kernel)
-#morphedDesertMask2 = cv2.morphologyEx(morphedDesertMask, cv2.MORPH_CLOSE, kernel)
 morphedMineMask = cv2.morphologyEx(mineMask, cv2.MORPH_CLOSE, kernel)
 morphedCrownMask = cv2.morphologyEx(crownMask, cv2.MORPH_OPEN, kernel)
 morphedCrownMask2 = cv2.morphologyEx(morphedCrownMask, cv2.MORPH_CLOSE, kernel)
@@ -74,11 +73,12 @@ morphedCrownMask2 = cv2.morphologyEx(morphedCrownMask, cv2.MORPH_CLOSE, kernel)
 
 '''
 Below we have three for loops. The first for loop iterates over each mask from mask list. 
-We then go through each tile in the mask(increase y and x with 100, every iteration and calculate an average
-bgr value. If the average bgr value is higher than 60 we implement the current mask number into the matrix
-called maskMatrix)
-When the loop is done, it prints the matrix with matching tile-type in the picture, ranging from 1 to 6
+We then go through each tile in the mask(increase y and x with 100, every iteration) and calculate an average
+bgr value within that range. If the average bgr value is higher than 70 we implement the current mask number 
+into the matrix at those coordinates, in the matrix called maskMatrix
+When the loop is done, it prints the matrix with matching tile-type in the picture, ranging from 1 to 7
 '''
+maskMatrix = np.zeros((5, 5), dtype=np.uint8)
 maskList = [morphedGrassMask, morphedWaterMask, morphedForestMask, morphedSandMask, morphedDesertMask, morphedCrownMask2, morphedMineMask]
 maskNumber = 0
 for maskNumber, mask in enumerate(maskList, 1):
@@ -91,7 +91,6 @@ for maskNumber, mask in enumerate(maskList, 1):
             tile = mask[y: y + 100, x: x + 100]
             if np.average(tile) >= 70:
                 maskMatrix[y1-1, x1-1] = maskNumber
-cv2.imshow("before", maskMatrix)
 print(maskMatrix)
 
 
@@ -100,19 +99,20 @@ def grassFire (newMaskMatrix, coordinates, currentId, tileValue):
     #we create a deque to append the coordinates we have burned
     #and create variables for keeping track of blobsize and crowns
     burnedQueue = deque([])
-    somethingBurned = False
     sizeOfBlob = 0
     crownCounter = 0
+    burned = False
 
+    #if the coordinate is equal to the current tilevalue, we append those coordinates to the deque
     if newMaskMatrix[coordinates[0], coordinates[1]] == tileValue:
         burnedQueue.append(coordinates)
-        somethingBurned = True
+        burned = True
 
     while len(burnedQueue) > 0:
         currentPosition = burnedQueue.pop()
         y, x = currentPosition
         if newMaskMatrix[y, x] == tileValue:
-        # Burn current_pos with current id and increment current blobSize and crowncounter
+        # Burn currentPosition with currentId and increment current blobSize and crowncounter
             sizeOfBlob += 1
             newMaskMatrix[y, x] = currentId
             crownCounter += templateMatching.crownMatrix[y, x]
@@ -127,16 +127,28 @@ def grassFire (newMaskMatrix, coordinates, currentId, tileValue):
             burnedQueue.append((y + 1, x))
         if (x + 1 < newMaskMatrix.shape[1]) and (newMaskMatrix[y, x + 1] == tileValue):
             burnedQueue.append((y, x + 1))
-
-    if somethingBurned:
+    #increment currentId with 10, after the blob has been located, to avoid reading the same blob again next time
+    if burned:
         currentId += 10
     return currentId, newMaskMatrix, tileValue, sizeOfBlob, crownCounter
 
 nextId = 10
-
 totalScoreCount = 0
-if maskMatrix[2, 2] == 6 or maskMatrix[2,2] == 0:
+'''
+if the middle tile has a crown in it(crown tile is number 6), we add 10 to the final score.
+the reason why we also say if maskmatrix[2,2] == 0 is because we don't have a mask for the crown tiles where there is
+a castle brick placed over it, and since we don't have a mask for that tile, the algorithm sets it to 0 
+'''
+if maskMatrix[2, 2] == 6 or maskMatrix[2, 2] == 0:
     totalScoreCount += 10
+
+'''
+We start with a for loop looping from 0 to 8. We start by looking for blobs which have a tileValue of 0, then 1, 
+and so on. We iterate through each coordinate with the grassFire algorithm on the maskMatrix.
+After each blob has been accounted for we take their size and multiply it with how many crowns were on that blob
+as well and add that to the totalScore, which we print out lastly.
+
+'''
 for i in range(8):
     for y, row in enumerate(maskMatrix):
         for x, pixel in enumerate(row):
@@ -155,7 +167,8 @@ print("Total score is:", totalScoreCount)
 
 
 while True:
-    cv2.imshow("Picture", picture0)
+    cv2.imshow("Picture", inputPicture)
+    cv2.imshow("GRass", morphedGrassMask)
     #cv2.imshow("CrownMask", morphedCrownMask2)
     #cv2.imshow("SAND", morphedSandMask)
     key = cv2.waitKey(1) #when the user presses esc key, the program shuts down
--- templateMatching.py
+++ templateMatching.py
@@ -2,8 +2,11 @@ import math
 from collections import deque
 import cv2
 import numpy as np
-
-picture = cv2.imread("Images/2.jpg", 0) #Change here to find the crowns in another photo!!
+'''
+load in our picture and our template. We rotate the template four times each time with 90 degrees since the crowns 
+can vary in their rotation.
+'''
+picture = cv2.imread("Images/3.jpg", 0) #Change here to find the crowns in another photo!!
 templateOriginal = cv2.imread("Images/EditedTemplateForcrown.png", 0)
 templateRotate90 = cv2.rotate(cv2.imread("Images/EditedTemplateForcrown.png", 0), cv2.ROTATE_90_CLOCKWISE)
 templateRotate180 = cv2.rotate(cv2.imread("Images/EditedTemplateForcrown.png", 0), cv2.ROTATE_180)
@@ -12,13 +15,13 @@ templateList = [templateOriginal, templateRotate90, templateRotate180, templateR
 crownMatrix = np.zeros((5, 5), np.uint8)
 w, h = templateOriginal.shape[::-1]
 
-# res = cv2.matchTemplate(picture, template, cv2.TM_CCOEFF_NORMED, mask = template)
-threshold = 0.68 # Secures how accurate the template should be in comparisson with the picture. set to 80%
-crownCoordinates = []
+
+threshold = 0.68 # Secures how accurate the template should be in comparison with the picture. set to 68%
+crownCoordinates = [] #create a list, to store the coordinates of where the crowns have been detected.
 coordinatesAreClose = False
-#loc = np.where(res >= threshold)
-for template in templateList: #itererer over de fire retninger af konge kronen
-    res = cv2.matchTemplate(picture, template, cv2.TM_CCOEFF_NORMED) #comparer vores almindelige billede med vores template
+
+for template in templateList: #We iterate over the four angles of the template stored in the templatelsit
+    res = cv2.matchTemplate(picture, template, cv2.TM_CCOEFF_NORMED) #Compare our image with the template
     loc = np.where(res >= threshold) #returns the x and y coordinates where template has matched if they are above the treshold
 
 
